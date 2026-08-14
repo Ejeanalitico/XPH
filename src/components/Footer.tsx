6a4cbc6cde645b3eb7f0082397fca92fd1fd6068
Ejeanalitico <pruebayfacturasph@gmail.com>
2026-08-14 16:09:13 -0600

    fix(brand): serve footer logo from local static asset

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ interface FooterProps {
   footerContact?: FooterContact;
 }
 
-const XPH_LOGO = '/api/logo?v=20260814-3';
+const XPH_LOGO = '/xph-logo.svg?v=20260814-4';
 
 export const Footer: React.FC<FooterProps> = ({
   onNavigateRoute,
